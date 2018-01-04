1d895813080065782f2302f071391eadf3fd38fd
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-04 14:14:39 -0500

    init

---
+++ SingleSignOn/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingleSignOn.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
